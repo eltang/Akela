d535967d0544f424e4eef2dbccacbe56631e3849
Gergely Nagy <algernon@madhouse-project.org>
2016-10-19 11:32:23 +0200

    KeyCodes: Added a bunch of more keycodes
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- lib/Akela/src/Akela/KeyCodes.h
+++ lib/Akela/src/Akela/KeyCodes.h
@@ -69,16 +69,44 @@ enum {
   KC_RBRC,
   KC_BSLS,
 
-  KC_QUOT = 0x034,
+  KC_SCLN = 0x033,
+  KC_QUOT,
   KC_GRV,
   KC_COMM,
   KC_DOT,
   KC_SLSH,
 
-  KC_F11 = 0x44,
+  KC_CAPS,
+
+  KC_F1,
+  KC_F2,
+  KC_F3,
+  KC_F4,
+  KC_F5,
+  KC_F6,
+  KC_F7,
+  KC_F8,
+  KC_F9,
+  KC_F10,
+  KC_F11,
+  KC_F12,
+
+  KC_PRSCR,
+  KC_SCLK,
+  KC_PAUSE,
+  KC_INS,
+  KC_HOME,
+  KC_PGUP,
+  KC_DEL,
+  KC_END,
+  KC_PGDN,
+  KC_RGHT,
+  KC_LEFT,
+  KC_DOWN,
+  KC_UP,
 
   KC_MPLY = 0xB0, // TODO
-  KC_MSTP = 0xB7,
+  KC_MSTP = 0xB7, // TODO
 
   KC_LCTL = 0xE0,
   KC_LSFT,
